97aa5cf229c6dcfa442e2dafdc8fdc9aa072fb80
ROUSE-prog <stevenr2049@gmail.com>
2026-05-08 10:18:05 -0400

    Forms

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,18 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import ShoppingList from "./ShoppingList";
-import Header from "./Header";
-import itemData from "../data/items";
+import ItemForm from "./ItemForm";
+import shoppingItems from "../data/items";
 
 function App() {
-  const [items, setItems] = useState(itemData);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [items, setItems] = useState(shoppingItems);
 
-  function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+  function handleItemFormSubmit(newItem) {
+    setItems((currentItems) => [...currentItems, newItem]);
   }
 
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+    <div className="App">
+      <ItemForm onItemFormSubmit={handleItemFormSubmit} />
       <ShoppingList items={items} />
     </div>
   );
--- src/components/ItemForm.jsx
+++ src/components/ItemForm.jsx
@@ -1,22 +1,43 @@
-import React from "react";
-import { v4 as uuid } from "uuid";
+import { useState } from "react";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Produce");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newItem = {
+      id: crypto.randomUUID(),
+      name,
+      category,
+    };
+
+    onItemFormSubmit(newItem);
+
+    setName("");
+    setCategory("Produce");
+  }
+
   return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
-      <label>
-        Category:
-        <select name="category">
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
+      <label htmlFor="category">Category</label>
+      <select
+        id="category"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value="Produce">Produce</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Dessert">Dessert</option>
+      </select>
 
       <button type="submit">Add to List</button>
     </form>
